52a76d36bf98ad1f811f9246e77414162f1d0458
Ruidy <ruidy.nemausat@gmail.com>
2024-04-27 00:46:51 +0200

    use typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: ["cupcake"],
   },
