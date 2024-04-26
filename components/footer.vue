d35253be03bba36ba380f7c4038b60eac86c7015
Ruidy <ruidy.nemausat@gmail.com>
2024-04-27 01:15:55 +0200

    basic layout

---
+++ components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <aside>
+      <p>Copyright &copy; 2024 - Made with ❤️</p>
+    </aside>
+  </footer>
+</template>
