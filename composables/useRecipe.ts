a89c2fcd2476cabe4908c0b26e1ac870212219c5
Ruidy <ruidy.nemausat@gmail.com>
2024-04-27 12:18:47 +0200

    fix: client side navigation

--- composables/useRecipe.ts
+++ composables/useRecipe.ts
@@ -7,22 +7,28 @@ type Recipe = {
 };
 
 export default async function () {
-  const { data, error } = await useAsyncData("random", () => {
-    const config = useRuntimeConfig();
-    return $fetch(`${config.apiUrl}random.php`);
-  });
+  const { data, pending, error } = await useAsyncData(
+    "random",
+    () => {
+      const config = useRuntimeConfig();
+      return $fetch(`${config.apiUrl}random.php`);
+    },
+    { lazy: true },
+  );
 
-  const tmp = data.value?.meals?.[0];
+  const tmp = computed(() => data.value?.meals?.[0]);
+
   const recipe = reactive<Recipe>({
-    title: tmp?.strMeal,
-    pictureUrl: tmp?.strMealThumb,
-    videoUrl: tmp?.strYoutube,
-    category: tmp?.strCategory,
-    origin: tmp?.strArea,
+    title: tmp.value.strMeal,
+    pictureUrl: tmp.value.strMealThumb,
+    videoUrl: tmp.value.strYoutube,
+    category: tmp.value.strCategory,
+    origin: tmp.value.strArea,
   });
 
   return {
     recipe,
+    pending,
     error,
   };
 }
