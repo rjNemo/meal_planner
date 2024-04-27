ec19f99492450149d9e1accb22c6b59560a2b5e2
Ruidy <ruidy.nemausat@gmail.com>
2024-04-27 18:31:15 +0200

    ingredients list

--- composables/useRecipe.ts
+++ composables/useRecipe.ts
@@ -4,6 +4,7 @@ type Recipe = {
   videoUrl: string;
   category: string;
   origin: string;
+  ingredients: { name: string; quantitiy: number };
 };
 
 export default async function () {
@@ -18,12 +19,25 @@ export default async function () {
 
   const tmp = computed(() => data.value?.meals?.[0]);
 
+  let names = [];
+  let quantities = [];
+  let i = 0;
+  for (const [k, v] of Object.entries(tmp.value)) {
+    if (k.startsWith("strIngredient") && !!v) {
+      names.push(v);
+    } else if (k.startsWith("strMeasure") && !!v) {
+      quantities.push(v);
+    }
+    i++;
+  }
+
   const recipe = reactive<Recipe>({
     title: tmp.value.strMeal,
     pictureUrl: tmp.value.strMealThumb,
     videoUrl: tmp.value.strYoutube,
     category: tmp.value.strCategory,
     origin: tmp.value.strArea,
+    ingredients: names.map((name, i) => ({ name, quantity: quantities[i] })),
   });
 
   return {
