e80b11ef17a6262b22942efcdf223eacfc707881
Ruidy <ruidy.nemausat@gmail.com>
2024-04-27 18:31:15 +0200

    ingredients list

--- pages/random.vue
+++ pages/random.vue
@@ -6,14 +6,18 @@ const { recipe, pending, error } = await useRecipe();
   <div v-if="pending">Loading</div>
   <div v-else-if="error">Failed: {{ error }}</div>
   <section v-else>
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <RecipeCard
-        :title="recipe.title"
-        :picture-url="recipe.pictureUrl"
-        :video-url="recipe.videoUrl"
-        :category="recipe.category"
-        :origin="recipe.origin"
-      />
+    <div class="lg:flex space-y-4 lg:justify-evenly py-4">
+      <div class="card w-96 bg-base-100 shadow-xl mx-auto lg:mx-2">
+        <RecipeCard
+          :title="recipe.title"
+          :picture-url="recipe.pictureUrl"
+          :video-url="recipe.videoUrl"
+          :category="recipe.category"
+          :origin="recipe.origin"
+        />
+      </div>
+
+      <RecipeIngredients :ingredients="recipe.ingredients" />
     </div>
   </section>
 </template>
