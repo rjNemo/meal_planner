83cc273e528bd3e3722020eb2301926db1364932
Ruidy <ruidy.nemausat@gmail.com>
2025-11-29 08:54:12 +0100

    chore: update deps

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -3,6 +3,6 @@ import { mergeRouters } from "../trpc";
 import { recipeRouter } from "./recipes";
 
 export const appRouter = mergeRouters(recipeRouter);
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
 export type RouterOutput = inferRouterOutputs<AppRouter>;
